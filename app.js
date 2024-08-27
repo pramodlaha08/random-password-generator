8fceabba7368f7da895851d9430f430feef310d3
Pramod Laha <pramodlaha.pm.08@gmail.com>
2024-08-27 16:17:12 +0545

    Uploaded a project

---
+++ app.js
@@ -0,0 +1,36 @@
+
+let passwordBox = document.getElementById('password');
+let generateBtn = document.querySelector('#generate');
+let copy = document.querySelector('#copy');
+
+let length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const number = "0123456789"
+const symbol = "!@#$%^&*()_+-={}[]><?/~";
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+
+}
+
+generateBtn.addEventListener('click', createPassword);
+
+function copyPassword() {
+    passwordBox.select();
+    document.execCommand("copy");
+}
+
+copy.addEventListener('click', copyPassword);
+
+
